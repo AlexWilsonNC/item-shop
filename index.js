10eb0569e7bcca1a5dfe25ea659b7cec858a137e
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-11-08 23:09:37 -0500

    initial commit w nav

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const modal = document.getElementById('entry-modal');
 const drop = document.getElementById('dropdown-content');
+const clicker = document.getElementById('nav-collapse');
 
 closeModal = () => {
     modal.style.display = 'none';
 }
 
-dropdown = () => {
-    if (drop.style.display === 'none') {
-    drop.style.display = 'flex';
-    drop.style.flexDirection = 'column';
+function dropdown() {
+    if (drop.style.visibility === 'hidden') {
+        drop.style.visibility = 'visible';
     } else {
-        drop.style.display = 'none';
+        drop.style.visibility = 'hidden';
     }
 }
+
+clicker.addEventListener('click', dropdown);
