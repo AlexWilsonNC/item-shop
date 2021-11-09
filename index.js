a7eed43d2597f1fec87ab59843533b1f5907de21
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-11-08 22:33:05 -0500

    initial commit w nav

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const modal = document.getElementById('entry-modal');
+const drop = document.getElementById('dropdown-content');
 
 closeModal = () => {
     modal.style.display = 'none';
+}
+
+dropdown = () => {
+    if (drop.style.display === 'none') {
+    drop.style.display = 'flex';
+    drop.style.flexDirection = 'column';
+    } else {
+        drop.style.display = 'none';
+    }
 }
